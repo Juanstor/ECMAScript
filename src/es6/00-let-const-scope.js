de32d921c633db587230bede49189fe8f2c13675
Juanstor <alvarezjuansbs@gmail.com>
2023-02-07 11:02:49 -0500

    actualizado

--- src/es6/00-let-const-scope.js
+++ src/es6/00-let-const-scope.js
@@ -1,10 +1,9 @@
 // Se declara y asigna un valor a la variable
 var lastName = 'David';
-
 // Se re asigna el valor 
 lastName = 'Oscar';
-
 console.log(lastName);
+
 
 
 // Variable con let
@@ -13,22 +12,24 @@ console.log(fruit);
 fruit = 'Kiwi';
 console.log(fruit);
 
+
+
 // 'Variable' con const (no se puede!! porque es una constante)
 const animal = 'Wolf';
 animal = 'raticate';
 console.log(animal);
 
 
+
+// Muestra la funcionalidad de scope, donde "var" es global y "const, let" solo se puede usar en el bloque de codigo.
 const fruits = () => {
     if(true) {
-        var fruit1 = 'Apple';
-        let fruit2 = 'Kiwi';
-        const fruit3 = 'Banana';
+        var fruit1 = 'Apple'; //Function scope
+        let fruit2 = 'Kiwi';  //block scope
+        const fruit3 = 'Banana'; //block scope
     }
-
     console.log(fruit1);
     console.log(fruit2);
     console.log(fruit3);
 }
-
-
+fruits();
--- src/es6/01-arrow-function.js
+++ src/es6/01-arrow-function.js
@@ -1,4 +1,18 @@
-// Una funcion normal
+// Una funcion normal:
 function square(num) {
     return num * num;
 }
+
+
+
+// Una funcion tipo arrow function:
+const square = (num) => {
+    return num * num;
+}
+
+
+
+// Una funcion arrow function pero simplificada:
+const square = num => num * num;
+//Notese que se pudo omitir los parentesis () para poner el parametro, y tambien los handelbars {} para el bloque de codigo, tambien se omite el return, ya que esta implicito.
+//Esto nos ayuda a simplificar codigo para funciones no tan largas o complejas.
