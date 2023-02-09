f3b44402527855fff20cf51b181a1e425b337da4
Juanstor <alvarezjuansbs@gmail.com>
2023-02-09 11:48:03 -0500

    set-add, last archive to ES6

---
+++ src/es6/-set-add.js
@@ -0,0 +1,20 @@
+const unaLista = new Set();
+
+unaLista.add('Articulo 1');
+unaLista.add('Articulo 1');
+unaLista.add('Articulo 2').add('Articulo 3').add('Articulo 4');
+unaLista.add('Articulo 4');
+unaLista.add('Articulo 5');
+
+
+console.log(unaLista); 
+
+/*
+Set(5) {
+    'Articulo 1',
+    'Articulo 2',
+    'Articulo 3',
+    'Articulo 4',
+    'Articulo 5'
+}
+*/
