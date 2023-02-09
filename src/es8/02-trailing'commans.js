32cfe712c9a64337a1fb1c2fdffc7a036e757fe3
Juanstor <alvarezjuansbs@gmail.com>
2023-02-09 16:01:32 -0500

    add trailin commands and star with es9

---
+++ src/es8/02-trailing'commans.js
@@ -0,0 +1,6 @@
+//  Las trailing commas consisten en comas al final de objetos o arrays que faciliten añadir nuevos elementos y evitar errores de sintaxis.
+
+const agesArrays = [24, 42, 18, , , 50, , 0];
+
+console.log(agesArrays);
+console.log(agesArrays.length);
