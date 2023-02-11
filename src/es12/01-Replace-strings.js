5534cd2c429f65c48ed0afa0356c8330bb081ef9
Juanstor <alvarezjuansbs@gmail.com>
2023-02-11 16:06:26 -0500

    add numeric separators + replace strings to folder es12

---
+++ src/es12/01-Replace-strings.js
@@ -0,0 +1,28 @@
+const inputText = "If you're :) and you know it clap your hands, :clap :clap"
+console.log(inputText);
+
+const emojiReplace = (text) => {
+  let emojized = ""
+  emojized = text.replaceAll(':)', '😀')
+  emojized = emojized.replaceAll(':clap', '👏🏻')
+
+  return emojized
+}
+
+console.log(emojiReplace(inputText))
+
+
+
+
+
+// ANOTHER EXAMPLE
+const mensaje = "JavaScript es maravilloso, con JavaScript puedo crear el futuro de la web."
+
+mensaje.replace("JavaScript", "Python")
+// 'Python es maravilloso, con JavaScript puedo crear el futuro de la web.'
+
+mensaje.replaceAll("JavaScript", "Python")
+// 'Python es maravilloso, con Python puedo crear el futuro de la web.'
+
+mensaje.replaceAll(/a/g, "*")
+// 'J*v*Script es m*r*villoso, con J*v*Script puedo cre*r el futuro de l* web.'
