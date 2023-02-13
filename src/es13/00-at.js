d51a098a4389dab0c60bf03fceeee8c21d375473
Juanstor <alvarezjuansbs@gmail.com>
2023-02-13 11:21:15 -0500

    installed fetch-node + 01 products & top level await

---
+++ src/es13/00-at.js
@@ -0,0 +1,8 @@
+const miArray = ["one", "two", "three", "four", "five", "six"];
+
+console.log(miArray[miArray.length - 1]); // six
+console.log(miArray.at(-1)); // six
+
+console.log(miArray.at(-2)); // five
+console.log(miArray.at(3)); // four
+console.log(miArray.at(0)); // one
---
+++ src/es13/01-products.mjs
@@ -0,0 +1,8 @@
+// api in platzi https://api.escuelajs.co/api/v1/products
+
+import fetch from "node-fetch"; 
+
+const response = await fetch("https://api.escuelajs.co/api/v1/products");
+const products = await response.json();
+
+export {products};
